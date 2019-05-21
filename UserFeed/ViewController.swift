1dfaba1f2137aa35e65306124a12c1884af43052
김민수 <whitewolf23@naver.com>
2019-05-21 14:12:40 +0900

    [dev] 2019 05 21
    add : 클로저를 이용한 serachController 와 tableview 초기화 그리고 간단한 tableview delegate 선언 -> lazy var 를 이용하여 지연변수를 적용 -> 해당 차이에 별차이가 없다면 좀더 쉽게 만들어야함
    Modify : 없음
    Refactory : navigation controller 를 이용하지 않고, searchBar UI를 이용하여, anchor 를 이용한 autolayout 적용
    Delete : 없음

--- UserFeed/ViewController.swift
+++ UserFeed/ViewController.swift
@@ -10,92 +10,78 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // cell identifier
-    let cellIdentifier = "cell"
+    let cellId = "cell"
     
-    // init tableview closure
+    let testArray = ["jason", "tom", "ash", "jay", "ellein", "semi"]
+    
+    
     lazy var tableView : UITableView = {
-        let tableView = UITableView()
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-
-        return tableView
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        return table
     }()
     
-    // search
-//    let seachBarController = UISearchController(searchResultsController: nil)
-
+    
     lazy var searchBar : UISearchBar = {
-        let searchBar = UISearchBar()
-        searchBar.translatesAutoresizingMaskIntoConstraints = false
-        return searchBar
+        let bar = UISearchBar()
+        bar.translatesAutoresizingMaskIntoConstraints = false
+//        bar.tintColor = .red
+        return bar
     }()
     
+    fileprivate func setupUI() {
+        view.addSubview(searchBar)
+        view.addSubview(tableView)
+  
+        let searchBarConstraints = [
+            searchBar.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
+            searchBar.heightAnchor.constraint(equalToConstant: 50),
+            searchBar.leftAnchor.constraint(equalTo: view.leftAnchor),
+            searchBar.rightAnchor.constraint(equalTo: view.rightAnchor),
+        ]
+        
+        let TableViewConstraints = [
+            tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 10),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ]
+        
+        NSLayoutConstraint.activate(searchBarConstraints)
+        NSLayoutConstraint.activate(TableViewConstraints)
+    }
     
-//    lazy var searchBarController : UISearchController = {
-//        let searchBarController = UISearchController(searchResultsController: nil)
-//        searchBarController.dimsBackgroundDuringPresentation = false
-//        searchBarController.hidesNavigationBarDuringPresentation = false
-//        searchBarController.searchBar.placeholder = "Search here..."
-//
-//        return searchBarController
-//    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        tableView.delegate = self
+        tableView.dataSource = self
         view.backgroundColor = .white
-        view.addSubview(tableView)
-        view.addSubview(searchBar)
-
-//        navigationItem.searchController = searchBarController
-        navigationItem.hidesSearchBarWhenScrolling = false
-
-        // tableview constraints
-        let constraints = [
-            
-            // top
-            searchBar.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-            // left
-            searchBar.heightAnchor.constraint(equalToConstant: 200),
-            // right
-            searchBar.leftAnchor.constraint(equalTo: view.leftAnchor),
-            // bottom
-            searchBar.rightAnchor.constraint(equalTo: view.rightAnchor),
-            
-            // top
-            tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
-            // left
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            // right
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            // bottom
-            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-        ]
-
-        // activate constraints
-        NSLayoutConstraint.activate(constraints)
-
-
     }
-    
     
 }
 
 
-
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return testArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
-        cell.textLabel?.text = "dsjfkajkljf"
+        
+        let data = testArray[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = data
         
         return cell
     }
+    
     
     
 }

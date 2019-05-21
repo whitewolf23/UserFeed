9de26b8976cce95ba9a440d6ea3b259745d97c26
김민수 <whitewolf23@naver.com>
2019-05-21 10:47:20 +0900

    [dev] 2019 05 21
    add : 클로저를 이용한 serachController 와 tableview 초기화 그리고 간단한 tableview delegate 선언
    Modify : 없음
    Refactory : 과제와는 다른 UI로 인해 searchBar 로 다시 변경 필요
    Delete : 없음

--- UserFeed/AppDelegate.swift
+++ UserFeed/AppDelegate.swift
@@ -17,6 +17,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window?.backgroundColor = .blue
+        window?.rootViewController = navigationController
+        
+//        if let window = window {
+//            window.backgroundColor = .blue
+//            window.rootViewController = navigationController
+//            window.makeKeyAndVisible()
+//        }
+        
         return true
     }
 
--- UserFeed/ViewController.swift
+++ UserFeed/ViewController.swift
@@ -9,12 +9,93 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // cell identifier
+    let cellIdentifier = "cell"
+    
+    // init tableview closure
+    lazy var tableView : UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
 
+        return tableView
+    }()
+    
+    // search
+//    let seachBarController = UISearchController(searchResultsController: nil)
+
+    lazy var searchBar : UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        return searchBar
+    }()
+    
+    
+//    lazy var searchBarController : UISearchController = {
+//        let searchBarController = UISearchController(searchResultsController: nil)
+//        searchBarController.dimsBackgroundDuringPresentation = false
+//        searchBarController.hidesNavigationBarDuringPresentation = false
+//        searchBarController.searchBar.placeholder = "Search here..."
+//
+//        return searchBarController
+//    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+        view.addSubview(searchBar)
 
- 
+//        navigationItem.searchController = searchBarController
+        navigationItem.hidesSearchBarWhenScrolling = false
+
+        // tableview constraints
+        let constraints = [
+            
+            // top
+            searchBar.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            // left
+            searchBar.heightAnchor.constraint(equalToConstant: 200),
+            // right
+            searchBar.leftAnchor.constraint(equalTo: view.leftAnchor),
+            // bottom
+            searchBar.rightAnchor.constraint(equalTo: view.rightAnchor),
+            
+            // top
+            tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            // left
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            // right
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            // bottom
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ]
+
+        // activate constraints
+        NSLayoutConstraint.activate(constraints)
+
+
+    }
+    
+    
 }
 
+
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel?.text = "dsjfkajkljf"
+        
+        return cell
+    }
+    
+    
+}
